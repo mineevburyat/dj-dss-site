badaf62dc0811862c53a7172a650bc4ffbf5d274
MineevHome <i@mineevburyat.ru>
2023-05-13 11:44:28 +0800

    fix errors

--- dss/app_services/urls.py
+++ dss/app_services/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ServiceViewByCategory
 
 app_name = 'app_services'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('str:<category>/', )
+    path('str:<category>/', ServiceViewByCategory.as_view(), name='categoryservice'),
 ]
--- dss/app_services/views.py
+++ dss/app_services/views.py
@@ -5,3 +5,15 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
     template_name = 'app_services/index.html'
     # title = "Магазин Store - главная"
+    
+class ServiceViewByCategory(TemplateView):
+    template_name = 'app_services/index_category.html'
+    # title = "Магазин Store - главная"
+    
+class SportSectionView(TemplateView):
+    template_name = 'app_services/index.html'
+    # title = "Магазин Store - главная"
+    
+class OtherServiceView(TemplateView):
+    template_name = 'app_services/index.html'
+    # title = "Магазин Store - главная"
