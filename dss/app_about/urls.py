70bc9564123577250759cfe7276c1dee597820af
Mineev <mineev.buryat@gmail.com>
2023-08-24 15:18:39 +0800

    about pages

--- dss/app_about/urls.py
+++ dss/app_about/urls.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView
+from .views import (IndexView,
+                    ManagmentView,
+                    DTurView,
+                    DocumentsView,
+                    ContactsView,
+                    VacantView,
+                    ZakupsView,
+                    ReseptionView)
 
 app_name = 'app_objects'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('managments/', ManagmentView.as_view(), name="managment"),
+    path('3dtur/', DTurView.as_view(), name="3dtur"),
+    path('documents/', DocumentsView.as_view(), name="documents"),
+    path('contacts/', ContactsView.as_view(), name="contacts"),
+    path('job/', VacantView.as_view(), name="job"),
+    path('purchases/', ZakupsView.as_view(), name="purchases"),
+    path('reseption/', ReseptionView.as_view(), name="reseption"),
 ]
--- dss/app_about/views.py
+++ dss/app_about/views.py
@@ -5,3 +5,24 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
     template_name = 'app_about/index.html'
     # title = "Магазин Store - главная"
+    
+class ManagmentView(TemplateView):
+    template_name = 'app_about/managment.html'
+    
+class DTurView(TemplateView):
+    template_name = 'app_about/3dtur.html'
+    
+class DocumentsView(TemplateView):
+    template_name = 'app_about/documents.html'
+    
+class ContactsView(TemplateView):
+    template_name = 'app_about/contacts.html'
+    
+class VacantView(TemplateView):
+    template_name = 'app_about/jobs.html'
+    
+class ZakupsView(TemplateView):
+    template_name = 'app_about/purchases.html'
+    
+class ReseptionView(TemplateView):
+    template_name = 'app_about/reseption.html'
